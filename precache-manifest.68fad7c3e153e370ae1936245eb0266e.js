56e7aca39852e3aa1f25764e40fc8e171e67613d
Alex-8-8 <46635183+Alex-8-8@users.noreply.github.com>
2020-04-26 01:30:28 +0300

    Updates

---
+++ precache-manifest.68fad7c3e153e370ae1936245eb0266e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651d9475fbc84eb63c32d1f43c56ff62",
+    "url": "/Harbour/index.html"
+  },
+  {
+    "revision": "d645a3623989ccc1aad2",
+    "url": "/Harbour/static/css/main.7d637473.chunk.css"
+  },
+  {
+    "revision": "79be775906bc6db1c065",
+    "url": "/Harbour/static/js/2.d2ab7a95.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Harbour/static/js/2.d2ab7a95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d645a3623989ccc1aad2",
+    "url": "/Harbour/static/js/main.4e02a43e.chunk.js"
+  },
+  {
+    "revision": "d423cb8e9d5b236880a9",
+    "url": "/Harbour/static/js/runtime-main.56375cb3.js"
+  },
+  {
+    "revision": "0fef3f687ae68a06150b3c040e0aabc5",
+    "url": "/Harbour/static/media/check-circle.0fef3f68.svg"
+  },
+  {
+    "revision": "02cda5bb29615a2d4017dcedae865c5d",
+    "url": "/Harbour/static/media/checkmark.02cda5bb.svg"
+  },
+  {
+    "revision": "2b5337e245b9400b7d19a9cada793f8e",
+    "url": "/Harbour/static/media/chevron.2b5337e2.svg"
+  },
+  {
+    "revision": "e91037e5a3420cc971b302ebef16b1a0",
+    "url": "/Harbour/static/media/error.e91037e5.svg"
+  },
+  {
+    "revision": "11f8bdd61091307c2b91b1a941549212",
+    "url": "/Harbour/static/media/no-icon.11f8bdd6.svg"
+  },
+  {
+    "revision": "454ea56ee9c3dd6c43e77e0ce2b48ebf",
+    "url": "/Harbour/static/media/search.454ea56e.svg"
+  }
+]);
