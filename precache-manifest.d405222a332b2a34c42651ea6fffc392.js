99f7328bbcc79135d1f6d622172a19225aeab78f
Alex-8-8 <46635183+Alex-8-8@users.noreply.github.com>
2020-04-26 13:08:43 +0300

    Updates

--- precache-manifest.d405222a332b2a34c42651ea6fffc392.js
+++ precache-manifest.d405222a332b2a34c42651ea6fffc392.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e624225be3e7208b9cf0b95b45b81df1",
+    "revision": "0a91597c594142cbb334aa271b039521",
     "url": "/Harbour/index.html"
   },
   {
-    "revision": "ca93ff32e5ede166c691",
+    "revision": "57a62b3dab50419f063b",
     "url": "/Harbour/static/css/main.7d637473.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Harbour/static/js/2.d2ab7a95.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ca93ff32e5ede166c691",
-    "url": "/Harbour/static/js/main.7a8f33d9.chunk.js"
+    "revision": "57a62b3dab50419f063b",
+    "url": "/Harbour/static/js/main.70814955.chunk.js"
   },
   {
     "revision": "d423cb8e9d5b236880a9",
